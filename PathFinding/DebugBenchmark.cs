8795e00afae8e8f114af3322036d689dd7a0ea44
Anderson Ramos <aramos@gazeus.com>
2016-06-07 13:29:14 -0300

    Adicionada classe de benchmark para contabilizar tempo do processo de FindPath, em debug mode;

---
+++ PathFinding/DebugBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PathFinding
+{
+	public class DebugBenchmark
+	{
+		private DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+
+		private double startTime;
+		private double endTime;
+
+		public double Duration {
+			get {
+				return endTime - startTime;
+			}
+		}
+
+		public DebugBenchmark() {
+			startTime = 0;
+			endTime = 0;
+		}
+
+		public void StartBenchmark() {
+			startTime = GetCurrentTimeStamp();
+		}
+
+		public void EndBenchmark() {
+			endTime = GetCurrentTimeStamp();
+		}
+
+		private double GetCurrentTimeStamp()
+		{
+			TimeSpan span = (DateTime.Now.ToLocalTime() - epoch);
+			return span.TotalSeconds;
+		}
+	}
+}
+
